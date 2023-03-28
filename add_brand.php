a66165fd1b1d3a25e49d2ea5ffe766c819439fdb
MadsenDev <chris@madsens.dev>
2023-03-28 22:45:57 +0200

    Further progress with db

---
+++ add_brand.php
@@ -0,0 +1,43 @@
+<?php
+// Establish a database connection
+$conn = mysqli_connect("localhost", "madsensd_madsen", "data2023", "madsensd_acct");
+
+// Get the form data
+$name = $_POST['name'];
+$info = $_POST['info'];
+$category = $_POST['category'];
+$active = isset($_POST['active']) ? 1 : 0;
+
+// Handle the file upload
+if (isset($_FILES['logo'])) {
+    $file_name = $_FILES['logo']['name'];
+    $file_tmp = $_FILES['logo']['tmp_name'];
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    $file_path = "uploads/" . uniqid() . "." . $file_ext;
+
+    if (move_uploaded_file($file_tmp, $file_path)) {
+        // Prepare the INSERT statement
+        $stmt = mysqli_prepare($conn, "INSERT INTO `brands` (`logo_path`, `name`, `info`, `category`, `active`) VALUES (?, ?, ?, ?, ?)");
+
+        // Bind the parameters to the statement
+        mysqli_stmt_bind_param($stmt, "ssbsi", $file_path, $name, $info, $category, $active);
+
+        // Execute the statement
+        if (mysqli_stmt_execute($stmt)) {
+            echo "Brand added successfully";
+            echo "<a href='add.html'>Add another one</a>";
+        } else {
+            echo "Error adding brand: " . mysqli_error($conn);
+        }
+
+        // Close the statement and the connection
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Error uploading logo: " . $_FILES['logo']['error'];
+    }
+} else {
+    echo "No logo uploaded";
+}
+
+mysqli_close($conn);
+?>
--- view_brands.php
+++ view_brands.php
@@ -3,17 +3,17 @@
 $conn = mysqli_connect("localhost", "madsensd_madsen", "data2023", "madsensd_acct");
 
 // Prepare the SELECT statement
-$stmt = mysqli_prepare($conn, "SELECT `id`, `logo`, `name`, `info` FROM `brands` WHERE `active` = 1");
+$stmt = mysqli_prepare($conn, "SELECT `id`, `logo_path`, `name`, `info` FROM `brands` WHERE `active` = 1");
 
 // Execute the statement
 mysqli_stmt_execute($stmt);
 
 // Bind the result variables
-mysqli_stmt_bind_result($stmt, $id, $logo, $name, $info);
+mysqli_stmt_bind_result($stmt, $id, $logo_path, $name, $info);
 
 // Loop through the results and display them
 while (mysqli_stmt_fetch($stmt)) {
-    echo "<img src='data:image/png;base64," . base64_encode($logo) . "' alt='" . $name . " logo'><br>";
+    echo "<img src='" . $logo_path . "' alt='" . $name . " logo'><br>";
     echo "<h2>" . $name . "</h2>";
     echo "<p>" . $info . "</p>";
 }
